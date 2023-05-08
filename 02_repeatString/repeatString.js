7060c6c860d5e82f12117057f611474f96453f68
SaleMisanovic <67553231+SaleMisanovic@users.noreply.github.com>
2023-05-08 23:24:50 +0200

    repeatString done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(phrase,number) {
+    let output = "";
+    if (number<0) {
+        return "ERROR"
+    }else{
+        for (let i = 0; i < number; i++) {
+            output+=phrase;
+        }
+    }
+    return output;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
